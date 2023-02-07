48d9fbc1b410921f34fc104a5d5e7696bcb1d9b8
Jonatas Pimenta <jonatas.pimenta@jal.corp.net>
2023-02-07 16:16:25 -0300

    Usando mysql falta configurar para deploy

--- setup/settings.py
+++ setup/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'alura_space',
+        'PORT': '3306',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'admin',
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
